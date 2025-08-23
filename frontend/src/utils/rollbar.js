aeb3c7ba3e8e8fc7ce14175cfaf702f3a43a15d7
Olesia <Swanriel@gmail.com>
2025-08-23 04:08:13 +0300

    Step11

---
+++ frontend/src/utils/rollbar.js
@@ -0,0 +1,10 @@
+import Rollbar from 'rollbar';
+
+const rollbar = new Rollbar({
+  accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  enabled: !!import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+});
+
+export default rollbar;
