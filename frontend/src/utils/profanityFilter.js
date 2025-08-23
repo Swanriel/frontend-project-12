88d564520bb0cca7f225fcfdce471444d2b57ff0
Olesia <Swanriel@gmail.com>
2025-08-23 06:33:59 +0300

    Add more words

--- frontend/src/utils/profanityFilter.js
+++ frontend/src/utils/profanityFilter.js
@@ -3,8 +3,11 @@ import * as Profanity from 'leo-profanity';
 Profanity.loadDictionary('en');
 Profanity.loadDictionary('ru');
 
+Profanity.add(['boobs', 'ass', 'fuck', 'shit', 'porn', 'sex']);
+
 export const filterProfanity = (text) => {
   if (!text || typeof text !== 'string') return text;
+  console.log('Filtering:', text, 'to:', Profanity.clean(text));
   return Profanity.clean(text);
 };
 
